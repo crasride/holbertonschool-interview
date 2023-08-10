dd0162c9f3b0e2183cf0bb982696bda9e9f1a733
crasride <jose.dfernandez@yahoo.fr>
2023-08-10 10:13:04 +0200

    check error

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -28,7 +28,7 @@ def canUnlockAll(boxes):
         # get the current box from the stack.
         for key in boxes[current_box]:
             # Iterate over the keys inside the current box.
-            if not visited[key]:
+            if key < n and not visited[key]:
                 # If the box corresponds to the key that has not been visited.
                 visited[key] = True
                 # Mark the box as visited.
@@ -49,3 +49,41 @@ if __name__ == "__main__":
 
     boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
     print(canUnlockAll(boxes3))
+
+    boxes6 = [
+        [10, 3, 8, 9, 6, 5, 8, 1],
+        [8, 5, 3, 7, 1, 8, 6],
+        [5, 1, 9, 1],
+        [],
+        [6, 6, 9, 4, 3, 2, 3, 8, 5],
+        [9, 4],
+        [4, 2, 5, 1, 1, 6, 4, 5, 6],
+        [9, 5, 8, 8],
+        [6, 2, 8, 6]
+    ]
+
+    print(canUnlockAll(boxes6))
+
+    boxes7 = [
+        [7, 5],
+        [1, 10, 7],
+        [9, 6, 10],
+        [7, 9],
+        [2],
+        [7, 3],
+        [7, 9, 10, 10, 8, 9, 2, 5],
+        [7, 2, 2, 4, 4, 2, 4, 8, 7],
+        [4, 2, 9, 6, 6, 5, 5],
+    ]
+
+    print(canUnlockAll(boxes7))
+    boxes1000 = []
+
+    keys = []
+    for n in range(1, 1000):
+        keys = []
+        for m in range(1, 1000):
+            keys.append(m)
+        boxes1000.append(keys)
+
+    print(canUnlockAll(boxes1000))
