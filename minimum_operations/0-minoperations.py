1f2ac283add9786bbabe8c06b819488f6f869e3d
crasride <jose.dfernandez@yahoo.fr>
2023-09-05 17:47:24 +0200

    task 0

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
+
+
 def minOperations(n):
+    """
+    Calculates minimum copy all and paste operations to reach n characters
+        Args:
+        n: characters to reach
+    Returns:
+        minimum needed operations
+    """
+    if n == 1:
+        return 0
+
+    operations = [0] * (n + 1)
+
+    for i in range(2, n + 1):
+        operations[i] = i # Initialize with the maximum number of operations
+
+        for j in range(2, int(i**0.5) + 1):
+            if i % j == 0:
+                operations[i] = min(operations[i], operations[j] + i // j)
+
+    return operations[n]
