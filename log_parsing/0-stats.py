ef3281418ac73a60c8d00b79dfb8a1755a6cc86e
crasride <jose.dfernandez@yahoo.fr>
2023-09-27 08:31:06 +0200

    task 0

---
+++ log_parsing/0-stats.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+import sys
+
+def print_stats(total_size, status_codes):
+    """
+    Print statistics including total file size and status code counts.
+    """
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
+
+def main():
+    total_size = 0
+    status_codes = {
+        200: 0,
+        301: 0,
+        400: 0,
+        401: 0,
+        403: 0,
+        404: 0,
+        405: 0,
+        500: 0,
+    }
+    line_count = 0
+
+    try:
+        for line in sys.stdin:
+            # Split the input line into parts
+            parts = line.split()
+            if len(parts) >= 7:
+                status_code = int(parts[-2])
+                file_size = int(parts[-1])
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
+                total_size += file_size
+                line_count += 1
+
+            # Print statistics every 10 lines
+            if line_count % 10 == 0:
+                print_stats(total_size, status_codes)
+
+    except KeyboardInterrupt:
+        pass  # Handle CTRL+C gracefully
+
+    finally:
+        # Print the final statistics
+        print_stats(total_size, status_codes)
+
+if __name__ == "__main__":
+    main()
+
