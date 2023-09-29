015dcacc7d1cde5571218ad5c7fdadd356032395
crasride <jose.dfernandez@yahoo.fr>
2023-09-29 18:58:42 +0200

    last check

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -3,66 +3,54 @@
 import sys
 
 
-#!/usr/bin/python3
-""" Script que lee stdin línea por línea """
-import sys
+# crate a dictionary to store the status
+status_dict = {'200': 0,
+               '301': 0,
+               '400': 0,
+               '401': 0,
+               '403': 0,
+               '404': 0,
+               '405': 0,
+               '500': 0}
 
-def main():
-# Crear un diccionario para almacenar los códigos de estado
-    status_dict = {
-        '200': 0,
-        '301': 0,
-        '400': 0,
-        '401': 0,
-        '403': 0,
-        '404': 0,
-        '405': 0,
-        '500': 0
-    }
+total_size = 0
+count = 0
 
-    total_size = 0
-    line_count = 0
+try:
+    for line in sys.stdin:
+        # split the line at each word
+        words_list = line.split(" ")
 
-    try:
-        for line in sys.stdin:
-            # Dividir la línea en palabras
-            words_list = line.split(" ")
+        if len(words_list) > 4:
+            file_size = int(words_list[-1])
+            status_code = words_list[-2]
 
-            if len(words_list) > 4:
-                file_size = int(words_list[-1])
-                status_code = words_list[-2]
+            # check status code in the dictionary and increment it
+            if status_code in status_dict.keys():
+                status_dict[status_code] += 1
 
-                # Verificar si el código de estado está en el diccionario
-                if status_code in status_dict:
-                    status_dict[status_code] += 1
+            #  total size
+            total_size += file_size
 
-                # Tamaño total
-                total_size += file_size
+            # add the line to the count
+            count += 1
 
-                # Añadir 1 al contador de líneas
-                line_count += 1
+        if count == 10:
+            # reset
+            count = 0
+            print('File size: {}'.format(total_size))
 
-            if line_count == 10:
-                # Reiniciar el contador de líneas
-                line_count = 0
-                print('Tamaño del archivo: {}'.format(total_size))
+            # print out status code counts, sorted by status code
+            for key, value in sorted(status_dict.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-                # Imprimir los recuentos de códigos de estado
-                for key, value in sorted(status_dict.items()):
-                    if value != 0:
-                        print('{}: {}'.format(key, value))
+except Exception as err:
+    pass
 
-    except Exception as err:
-        pass
-
-    finally:
-        # Imprimir el tamaño total del archivo
-        print('Tamaño del archivo: {}'.format(total_size))
-        
-        # Imprimir los recuentos de códigos de estado,
-        for key, value in sorted(status_dict.items()):
-            if value != 0:
-                print('{}: {}'.format(key, value))
-
-if __name__ == "__main__":
-    main()
+finally:
+    # total size
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(status_dict.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
