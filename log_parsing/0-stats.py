23d7aacf9e52469292793f25e8fc47e4b38b4fe8
crasride <jose.dfernandez@yahoo.fr>
2023-09-29 18:27:52 +0200

    last check

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,12 +1,9 @@
-#!/usr/bin/python3
-""" Script that reads stdin line """
 import sys
 import traceback
 
-# Definir los índices para el tamaño del archivo y el código de estado
+# Define the indices for the size of the file and the status code
 FILE_SIZE_INDEX = -1
 STATUS_CODE_INDEX = -2
-
 
 def print_stats(total_size, status_codes):
     """ Print statistics including total file size and status code counts """
@@ -14,7 +11,6 @@ def print_stats(total_size, status_codes):
     for code in sorted(status_codes.keys()):
         if status_codes[code] > 0:
             print("{}: {}".format(code, status_codes[code]))
-
 
 def main():
     """ Parses a log """
@@ -41,6 +37,7 @@ def main():
                     status_code = int(parts[STATUS_CODE_INDEX])
                     file_size = int(parts[FILE_SIZE_INDEX])
                 except (ValueError, IndexError):
+                    print("Skipped line due to wrong format: {}".format(line.strip()))
                     continue
                 if status_code in status_codes:
                     status_codes[status_code] += 1
@@ -63,3 +60,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
