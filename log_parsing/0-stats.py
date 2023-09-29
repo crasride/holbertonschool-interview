404b0dad56fee8ad1d1a243431774cfa9bcfa63f
crasride <jose.dfernandez@yahoo.fr>
2023-09-29 18:57:01 +0200

    last check

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,66 +1,68 @@
 #!/usr/bin/python3
 """ Script que lee stdin línea por línea """
 import sys
-import traceback
-
-# Definir los índices para el tamaño del archivo y el código de estado
-FILE_SIZE_INDEX = -1
-STATUS_CODE_INDEX = -2
 
 
-def print_stats(total_size, status_codes):
-    """ Imprimir estadísticas, incluyendo el tamaño total del archivo y el recuento de códigos de estado """
-    print("Tamaño del archivo: {}".format(total_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
-
+#!/usr/bin/python3
+""" Script que lee stdin línea por línea """
+import sys
 
 def main():
-    """ Analizar un registro """
-    total_size = 0
-    status_codes = {
-        200: 0,
-        301: 0,
-        400: 0,
-        401: 0,
-        403: 0,
-        404: 0,
-        405: 0,
-        500: 0,
+# Crear un diccionario para almacenar los códigos de estado
+    status_dict = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
     }
+
+    total_size = 0
     line_count = 0
 
     try:
-        """ Leer stdin línea por línea """
         for line in sys.stdin:
-            # Dividir la línea de entrada en partes
-            parts = line.split(" ")
-            if len(parts) >= 7:
-                try:
-                    status_code = int(parts[STATUS_CODE_INDEX])
-                    file_size = int(parts[FILE_SIZE_INDEX])
-                except (ValueError, IndexError):
-                    continue
-                if status_code in status_codes:
-                    status_codes[status_code] += 1
+            # Dividir la línea en palabras
+            words_list = line.split(" ")
+
+            if len(words_list) > 4:
+                file_size = int(words_list[-1])
+                status_code = words_list[-2]
+
+                # Verificar si el código de estado está en el diccionario
+                if status_code in status_dict:
+                    status_dict[status_code] += 1
+
+                # Tamaño total
                 total_size += file_size
+
+                # Añadir 1 al contador de líneas
                 line_count += 1
 
-            # Imprimir estadísticas cada 10 líneas
-            if line_count % 10 == 0:
-                print_stats(total_size, status_codes)
+            if line_count == 10:
+                # Reiniciar el contador de líneas
+                line_count = 0
+                print('Tamaño del archivo: {}'.format(total_size))
 
-    except KeyboardInterrupt:
-        """ Interrupción de teclado """
-        traceback.print_exc()  # Imprimir la traza cuando se interrumpe
-        sys.exit(1)  # Salir con un código de estado no cero
+                # Imprimir los recuentos de códigos de estado
+                for key, value in sorted(status_dict.items()):
+                    if value != 0:
+                        print('{}: {}'.format(key, value))
+
+    except Exception as err:
+        pass
 
     finally:
-        """ """
-        # Imprimir las estadísticas finales
-        print_stats(total_size, status_codes)
+        # Imprimir el tamaño total del archivo
+        print('Tamaño del archivo: {}'.format(total_size))
+        
+        # Imprimir los recuentos de códigos de estado,
+        for key, value in sorted(status_dict.items()):
+            if value != 0:
+                print('{}: {}'.format(key, value))
 
 if __name__ == "__main__":
     main()
-
