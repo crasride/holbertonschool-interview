a3115b737043ae87d97a6900449809ebf53bcfd9
crasride <jose.dfernandez@yahoo.fr>
2023-09-29 19:08:31 +0200

    last check

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -3,46 +3,29 @@
 import sys
 
 
-
-status_codes ={
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0,
-    }
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
+                "405": 0, "500": 0}
 
 total_size = 0
 count = 0
 
 try:
     for line in sys.stdin:
-        # split the line at each word
         parts = line.split(" ")
 
         if len(parts) > 4:
             file_size = int(parts[-1])
             status_code = parts[-2]
-
-            # check status code in the dictionary and increment it
             if status_code in status_codes.keys():
                 status_codes[status_code] += 1
 
-            #  total size
             total_size += file_size
-
-            # add the line to the count
             count += 1
 
         if count == 10:
-            # reset
             count = 0
             print('File size: {}'.format(total_size))
 
-            # print out status code counts, sorted by status code
             for key, value in sorted(status_codes.items()):
                 if value != 0:
                     print('{}: {}'.format(key, value))
