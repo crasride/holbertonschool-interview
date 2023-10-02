a5c0bb56046059277f95d4966fd8107e0b514106
crasride <jose.dfernandez@yahoo.fr>
2023-10-02 10:05:44 +0200

    task 0

---
+++ linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @head: pointer to the head of the linked list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *head)
+{
+	listint_t *current = head;
+	listint_t *current_fast = head;
+
+	while (current && current_fast)
+	{
+		/* Move the current pointer one step at a time */
+		current = current->next;
+
+		/* Move the current_fast pointer two steps at a time if possible */
+		if (current_fast->next)
+			current_fast = current_fast->next->next;
+		else
+			current_fast = NULL;
+
+		/* If the current and current_fast pointers meet, there is a cycle */
+		if (current == current_fast)
+			return (1);
+	}
+
+	/* If we reach the end of the list, there is no cycle */
+	return (0);
+}
+
+
