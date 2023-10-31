ac6048e0df531cea7817ab1e197e1d04a7853565
crasride <jose.dfernandez@yahoo.fr>
2023-10-31 09:17:00 +0100

    add functions

--- slide_line/slide_line.h
+++ slide_line/slide_line.h
@@ -5,8 +5,10 @@
 #define SLIDE_RIGHT 1
 
 #include <stddef.h>
-#include <stdlib.h>
 
+/* Funtions task 0 */
+void slide_left(int *line, size_t size);
+void slide_right(int *line, size_t size);
 int slide_line(int *line, size_t size, int direction);
 
 #endif /* SLIDE_LINE_H */
