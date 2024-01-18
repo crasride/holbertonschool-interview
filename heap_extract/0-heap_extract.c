d0484196a74acc925a0917d187f805995c9178aa
crasride <jose.dfernandez@yahoo.fr>
2024-01-18 10:09:26 +0100

    add comment code

--- heap_extract/0-heap_extract.c
+++ heap_extract/0-heap_extract.c
@@ -11,22 +11,29 @@ int heap_extract(heap_t **root)
 	if (root == NULL || *root == NULL)
 		return (0);
 
+	/* Extract the value from the root node */
 	int extracted_value = (*root)->n;
+	/* Find the last level-order node in the heap */
 	heap_t *last_node = find_last_node(*root);
 
+	/* If the last node is not the root */
 	if (last_node != *root)
 	{
+		/* Copy the value of the last node to the root */
 		(*root)->n = last_node->n;
+		/* Update the parent's pointer to the last node to NULL */
 		if (last_node->parent->left == last_node)
 			last_node->parent->left = NULL;
 		else
 			last_node->parent->right = NULL;
 
 		free(last_node);
+		/* Restore the Max Heap property */
 		heapify_down(*root);
 	}
 	else
 	{
+	/* If the root is also the last node, free the root and set it to NULL */
 		free(*root);
 		*root = NULL;
 	}
@@ -48,17 +55,18 @@ heap_t *find_last_node(heap_t *root)
 	heap_t *last_node = NULL;
 	heap_t **queue = malloc(sizeof(heap_t *) * 1024);
 
+	/* Check if memory allocation fails */
 	if (queue == NULL)
 		exit(EXIT_FAILURE);
 
 	size_t front = 0, rear = 0;
-
+	/* Enqueue the root */
 	queue[rear++] = root;
-
+	/* Breadth-first search to find the last node */
 	while (front < rear)
 	{
 		last_node = queue[front];
-
+	/* Enqueue left and right children if they exist */
 		if (last_node->left)
 			queue[rear++] = last_node->left;
 		if (last_node->right)
@@ -85,19 +93,21 @@ void heapify_down(heap_t *root)
 	heap_t *left = root->left;
 	heap_t *right = root->right;
 
+	/* Identify the largest node among the root, left, and right */
 	if (left && left->n > largest->n)
 		largest = left;
 
 	if (right && right->n > largest->n)
 		largest = right;
 
+	/* If the largest node is not the root, swap values continue heapifying */
 	if (largest != root)
 	{
 		int temp = root->n;
 
 		root->n = largest->n;
 		largest->n = temp;
-
+		/* Recursively heapify down the affected subtree */
 		heapify_down(largest);
 	}
 }
