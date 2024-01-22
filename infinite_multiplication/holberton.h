9ab7366dbc5ae10fc7d4d273a8b8b7b579c1bc5b
crasride <jose.dfernandez@yahoo.fr>
2024-01-22 09:35:26 +0100

    Prototypes

---
+++ infinite_multiplication/holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Prototypes Task 0*/
+int _putchar(char c);
+void error_exit(void);
+int is_digit(char *str);
+void multiply(char *num1, char *num2);
+
+#endif /* HOLBERTON_H */
