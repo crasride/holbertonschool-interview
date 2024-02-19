c8c80268117bd8e3f3c387b30f3db218e4c3a500
crasride <jose.dfernandez@yahoo.fr>
2024-02-19 09:12:35 +0100

    h

---
+++ merge_sort/sort.h
@@ -0,0 +1,11 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Functions Tasks 0*/
+void print_array(const int *array, size_t size);
+void merge_sort(int *array, size_t size);
+
+#endif /* SORT_H */
