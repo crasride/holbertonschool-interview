804de348e709a6ce2d019e74c178453ef1aa53fa
crasride <jose.dfernandez@yahoo.fr>
2024-03-04 08:50:37 +0100

    add comment

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -9,11 +9,14 @@
 */
 List *add_node_end(List **list, char *str)
 {
+	/* allocate memory for a new node */
 	List *new_node = malloc(sizeof(List));
 
+	/* if malloc fails, return NULL */
 	if (!new_node)
 		return (NULL);
 
+	/* returns a pointer to a new string which is a duplicate of the string str*/
 	new_node->str = strdup(str);
 	if (!new_node->str)
 	{
@@ -21,12 +24,14 @@ List *add_node_end(List **list, char *str)
 		return (NULL);
 	}
 
+	/* if the list is empty, the new node is the only node in the list */
 	if (*list == NULL)
 	{
 		new_node->prev = new_node;
 		new_node->next = new_node;
 		*list = new_node;
 	}
+	/* if the list is not empty, the new node is added to the end of the list */
 	else
 	{
 		new_node->prev = (*list)->prev;
@@ -48,24 +53,28 @@ List *add_node_end(List **list, char *str)
 */
 List *add_node_begin(List **list, char *str)
 {
+	/* allocate memory for a new node */
 	List *new_node = malloc(sizeof(List));
 
+	/* if malloc fails, return NULL */
 	if (!new_node)
 		return (NULL);
 
+	/* returns a pointer to a new string which is a duplicate of the string str*/
 	new_node->str = strdup(str);
 	if (!new_node->str)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
+	/* if the list is empty, the new node is the only node in the list */
 	if (*list == NULL)
 	{
 		new_node->prev = new_node;
 		new_node->next = new_node;
 		*list = new_node;
 	}
+	/*the list is not empty, the new node is added to the beginning of the list */
 	else
 	{
 		new_node->prev = (*list)->prev;
