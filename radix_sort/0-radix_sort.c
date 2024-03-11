6ad6dd604a4b6dc3fdb5c61561e0601a744d1348
crasride <jose.dfernandez@yahoo.fr>
2024-03-11 09:02:49 +0100

    add comment

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -11,6 +11,7 @@ int getMax(int *array, size_t size)
 	int max = array[0];
 	size_t i;
 
+	/* Find the maximum value in the array */
 	for (i = 1; i < size; i++)
 	{
 		if (array[i] > max)
@@ -35,18 +36,22 @@ void countingSort(int *array, size_t size, int exp)
 	if (!output)
 		return;
 
+	/* Counting the frequency of each digit */
 	for (i = 0; i < size; i++)
 		count[(array[i] / exp) % 10]++;
 
+	/* Cumulative sum to determine final positions */
 	for (i = 1; i < 10; i++)
 		count[i] += count[i - 1];
 
+	/* Placing elements in the output array in the correct order */
 	for (i = size - 1; i < SIZE_MAX; i--)
 	{
 		output[count[(array[i] / exp) % 10] - 1] = array[i];
 		count[(array[i] / exp) % 10]--;
 	}
 
+	/* Copy the sorted elements into the original array */
 	for (i = 0; i < size; i++)
 		array[i] = output[i];
 
@@ -62,14 +67,19 @@ void radix_sort(int *array, size_t size)
 {
 	int max, exp;
 
+	/* Check for NULL or empty array */
 	if (!array || size < 2)
 		return;
 
+	/* Get the maximum value in the array */
 	max = getMax(array, size);
 
+	/* Iterate through each digit, starting with the least significant */
 	for (exp = 1; max / exp > 0; exp *= 10)
 	{
+	/*Call countingSort to sort the array based on the current digit*/
 		countingSort(array, size, exp);
+		/* Print the array after each iteration */
 		print_array(array, size);
 	}
 }
