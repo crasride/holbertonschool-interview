8afc8333eb94b4b06ed8cc9962e1dea4a7a44343
crasride <jose.dfernandez@yahoo.fr>
2024-03-11 08:52:06 +0100

    fucntions

---
+++ radix_sort/sort.h
@@ -0,0 +1,12 @@
+#ifndef _SORT_H_
+#define _SORT_H_
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+/* Functions Tasks 0*/
+void print_array(const int *array, size_t size);
+void radix_sort(int *array, size_t size);
+
+#endif /* _SORT_H_ */
