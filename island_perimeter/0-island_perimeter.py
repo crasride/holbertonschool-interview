3fcb8fabcc7870d7c803c453632bef326f74f29d
crasride <jose.dfernandez@yahoo.fr>
2024-03-18 08:52:31 +0100

    tasks 0

---
+++ island_perimeter/0-island_perimeter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+0-island_perimeter module
+"""
+
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid.
+
+    Args:
+        grid (list[list[int]]): A list of list of integers representing the grid.
+
+    Returns:
+        int: The perimeter of the island.
+
+    """
+
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
