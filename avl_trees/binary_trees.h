53fbaa1f556f43413f46ff05a76c99c05bfddb60
crasride <jose.dfernandez@yahoo.fr>
2024-03-25 09:16:47 +0100

    modify struc

--- avl_trees/binary_trees.h
+++ avl_trees/binary_trees.h
@@ -8,26 +8,19 @@
 #include <limits.h>
 
 
-/**
-* struct binary_tree_s - Binary tree node
-*
-* @n: Integer stored in the node
-* @parent: Pointer to the parent node
-* @left: Pointer to the left child node
-* @right: Pointer to the right child node
-*/
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
 	struct binary_tree_s *right;
+	struct binary_tree_s *left;
 };
 
+/* Binary Search Tree */
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
 
-/* Prototype for the function Task 0 */
+/* Protoypes Functions Tasks0*/
 void binary_tree_print(const binary_tree_t *);
 int binary_tree_is_avl(const binary_tree_t *tree);
 
