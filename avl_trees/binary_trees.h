f7ed16e65846971ebe8b5b201fc824bd1f232c34
crasride <jose.dfernandez@yahoo.fr>
2024-03-25 09:13:29 +0100

    modify struc

--- avl_trees/binary_trees.h
+++ avl_trees/binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef _AVL_TREES_H_
+#define _AVL_TREES_H_
 
 #include <stddef.h>
 #include <stdio.h>
@@ -22,7 +22,7 @@ typedef struct binary_tree_s
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-}
+};
 
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
@@ -31,4 +31,4 @@ typedef struct binary_tree_s avl_t;
 void binary_tree_print(const binary_tree_t *);
 int binary_tree_is_avl(const binary_tree_t *tree);
 
-#endif /* _BINARY_TREES_H_ */
+#endif /* _AVL_TREES_H_ */
