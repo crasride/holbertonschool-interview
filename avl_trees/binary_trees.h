45b391a553bc96cc1b0223cdaa408cc97a0a7654
crasride <jose.dfernandez@yahoo.fr>
2024-03-25 09:17:47 +0100

    modify struc2

--- avl_trees/binary_trees.h
+++ avl_trees/binary_trees.h
@@ -7,7 +7,14 @@
 #include <string.h>
 #include <limits.h>
 
-
+/**
+* struct binary_tree_s - Binary tree node
+*
+* @n: Integer stored in the node
+* @parent: Pointer to the parent node
+* @left: Pointer to the left child node
+* @right: Pointer to the right child node
+*/
 struct binary_tree_s
 {
 	int n;
