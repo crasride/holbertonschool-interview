3373477580059fa3dd3dc75e76bfba4b3bf167ad
crasride <jose.dfernandez@yahoo.fr>
2024-03-25 08:56:54 +0100

    prototypes functions

---
+++ avl_trees/binary_trees.h
@@ -0,0 +1,34 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+
+/**
+* struct binary_tree_s - Binary tree node
+*
+* @n: Integer stored in the node
+* @parent: Pointer to the parent node
+* @left: Pointer to the left child node
+* @right: Pointer to the right child node
+*/
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s avl_t;
+
+/* Prototype for the function Task 0 */
+void binary_tree_print(const binary_tree_t *);
+int binary_tree_is_avl(const binary_tree_t *tree);
+
+#endif /* _BINARY_TREES_H_ */
