6d2ca08a7080cd590c6e1bf833fdc3a2218fe324
crasride <jose.dfernandez@yahoo.fr>
2024-04-15 08:53:00 +0200

    prototype

---
+++ regex/regex.h
@@ -0,0 +1,11 @@
+#ifndef _REGEX_H
+#define _REGEX_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Prototypes Functions */
+int regex_match(char const *str, char const *pattern);
+
+
+#endif /* _REGEX_H */
