7dfc7f6e5006c6275aa64073b0ac7eb9daa8cd0a
crasride <jose.dfernandez@yahoo.fr>
2024-04-22 09:39:24 +0200

    Prototype Function

---
+++ substring/substring.h
@@ -0,0 +1,12 @@
+#ifndef _SUBSTRING_H
+#define _SUBSTRING_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+/* Prototype Function */
+int *find_substring(char const *s, char const **words, int nb_words, int *n);
+
+#endif /* _SUBSTRING_H */
