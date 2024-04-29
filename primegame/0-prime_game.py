6f8b4cce6c8753a9d62be05d81d4d148b4dc1f14
crasride <jose.dfernandez@yahoo.fr>
2024-04-29 09:30:27 +0200

    task 0

---
+++ primegame/0-prime_game.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num <= 3:
+        return True
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def isWinner(x, nums):
+    winner = None
+    for i in range(x):
+        num = nums[i]
+        prime_count = sum(1 for j in range(2, num + 1) if is_prime(j))
+        if prime_count % 2 == 0:
+            winner = "Ben" if i % 2 == 0 else "Maria"
+        else:
+            winner = "Maria" if i % 2 == 0 else "Ben"
+    return winner
+
+
+
