db7bb78a9d962e0ae632ad05b42f03009ce63085
crasride <jose.dfernandez@yahoo.fr>
2024-04-29 10:46:31 +0200

    add comment modify algorithmo

--- primegame/0-prime_game.py
+++ primegame/0-prime_game.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
 """
-Prime Game
+Prime Game: Determines the winner of a game where players remove prime numbers
+and their multiples from a set of consecutive integers.
 """
 
 
-def is_prime(num):
-    """ Returns True if num is prime, False otherwise """
-    if num <= 1:
-        return False
-    if num <= 3:
-        return True
-    if num % 2 == 0 or num % 3 == 0:
-        return False
-    i = 5
-    while i * i <= num:
-        if num % i == 0 or num % (i + 2) == 0:
-            return False
-        i += 6
-    return True
+def sieve_of_eratosthenes(limit):
+    """
+    Generates a list of primes using Sieve of Eratosthenes
+    Finding Prime numbers
+    https://www.youtube.com/watch?v=eKp56OLhoQs
+    """
+    # Initialize a list to mark numbers as prime or not
+    primes = [True] * (limit + 1)
+    primes[0] = primes[1] = False  # 0 and 1 are not prime
+    p = 2
+    # Iterate over numbers until the square root of the limit
+    while p * p <= limit:
+        if primes[p]:
+            # Mark multiples of p as not prime
+            for i in range(p * p, limit + 1, p):
+                primes[i] = False
+        p += 1
+    return primes
 
 
 def isWinner(x, nums):
@@ -25,15 +30,21 @@ def isWinner(x, nums):
     if x <= 0:
         return None
 
+    # Generate list of primes up to the maximum number in nums
+    primes = sieve_of_eratosthenes(max(max(nums), 2))
     ben_wins = 0
     maria_wins = 0
+    # Iterate over the given numbers
     for num in nums:
-        prime_count = sum(1 for j in range(2, num + 1) if is_prime(j))
+        # Count the number of primes up to num
+        prime_count = sum(1 for j in range(2, num + 1) if primes[j])
+        # Determine the winner of the round based on the count of primes
         if prime_count % 2 == 0:
             ben_wins += 1
         else:
             maria_wins += 1
 
+    # Decide the overall winner based on the total wins
     if ben_wins > maria_wins:
         return "Ben"
     elif maria_wins > ben_wins:
